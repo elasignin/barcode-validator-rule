21b0a11ee986c41298bf122a2367b2460b439506
admin <>
2024-08-29 09:05:01 +0000

    {/src/main/java/com/ingramcontent/consolidated/labels/model/BarcodeDetail.java}

--- src/main/java/com/ingramcontent/consolidated/labels/model/BarcodeDetail.java
+++ src/main/java/com/ingramcontent/consolidated/labels/model/BarcodeDetail.java
@@ -6,12 +6,37 @@ package com.ingramcontent.consolidated.labels.model;
 
 public class BarcodeDetail implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public BarcodeDetail() {
-    }
+	private com.ingramcontent.consolidated.labels.model.BarcodeValidator barcodeValidator;
+	private com.ingramcontent.consolidated.labels.model.ToteInformation toteInformation;
 
+	public BarcodeDetail() {
+	}
 
+	public com.ingramcontent.consolidated.labels.model.BarcodeValidator getBarcodeValidator() {
+		return this.barcodeValidator;
+	}
 
+	public void setBarcodeValidator(
+			com.ingramcontent.consolidated.labels.model.BarcodeValidator barcodeValidator) {
+		this.barcodeValidator = barcodeValidator;
+	}
+
+	public com.ingramcontent.consolidated.labels.model.ToteInformation getToteInformation() {
+		return this.toteInformation;
+	}
+
+	public void setToteInformation(
+			com.ingramcontent.consolidated.labels.model.ToteInformation toteInformation) {
+		this.toteInformation = toteInformation;
+	}
+
+	public BarcodeDetail(
+			com.ingramcontent.consolidated.labels.model.BarcodeValidator barcodeValidator,
+			com.ingramcontent.consolidated.labels.model.ToteInformation toteInformation) {
+		this.barcodeValidator = barcodeValidator;
+		this.toteInformation = toteInformation;
+	}
 
 }
